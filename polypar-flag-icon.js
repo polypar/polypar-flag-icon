85f7695cc7769fca40e768cf42973a9d1557a169
Ismael Civera Pardo <iscipar@hotmail.com>
2019-07-16 01:31:33 +0200

    First commit

---
+++ polypar-flag-icon.js
@@ -0,0 +1,9 @@
+import { LitElement, html } from 'lit-element';
+
+export class PolyparFlagIcon extends LitElement {
+
+    render() {
+        return html``;
+    }
+}
+customElements.define('polypar-flag-icon', PolyparFlagIcon);
