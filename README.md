cb49d29597f124cc1a46db10f692795073343978
Ismael Civera Pardo <iscipar@hotmail.com>
2019-07-16 17:46:19 +0200

    Component created

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
 # polypar-flag-icon
 
 Web component for SVG icons of country flags based on LitElement
+
+```
+<polypar-flag-icon flag="kingdom"></polypar-flag-icon>
+```
+
+## Flags available
+
+The flag property accepts the countries listed bellow:
+
+- france
+- italy
+- kingdom
+
+## Styling
+
+The following custom properties and mixins are available for styling:
+
+Custom property | Description | Default
+----------------|-------------|---------
+--polypar-flag-icon-size | icon size (width = height) | 24px
--- polypar-flag-icon.js
+++ polypar-flag-icon.js
@@ -1,9 +1,50 @@
-import { LitElement, html } from 'lit-element';
+import { LitElement, html, css } from 'lit-element';
+import { icons } from './icons';
 
 export class PolyparFlagIcon extends LitElement {
 
+    static get properties() {
+        return {
+            flag: {
+                type: String
+            }
+        };
+    }
+
+    constructor() {
+        super();
+        this.flag = 'kingdom';
+    }
+
+    static get styles() {
+        return css`
+            :host {
+                display: inline-block;
+            }
+
+            :host([hidden]) {
+                display: none;
+            }
+          
+            path[fill="none"] {
+                fill: transparent;
+            }
+
+            svg {
+                width: var(--polypar-flag-icon-size, 24px);
+                height: var(--polypar-flag-icon-size, 24px);
+            }
+
+            div {
+                display: flex;
+            }
+        `;
+    }
+
     render() {
-        return html``;
+        return html`
+            ${icons[this.flag]}
+        `;
     }
 }
 customElements.define('polypar-flag-icon', PolyparFlagIcon);
